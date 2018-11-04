520412d6aa2825b436e83d1719de31ee473b9b43
charlotteheggem <charlotteheggem@hotmail.com>
2018-11-04 13:28:56 +0000

    changes to MCL motionupdate and main

--- scripts/MCL.py
+++ scripts/MCL.py
@@ -4,7 +4,10 @@ from math import sqrt, exp, pi
 from scipy.integrate import quad
 import rospy
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose
 from geometry_msgs.msg import poseArray
+
+
 
 class Particle(object):
 	def __init__(self,x,y,theta):
@@ -63,7 +66,45 @@ class ParticleFilter(object):
 			return 0
 
 
-	#measurement model
+    def motionUpdate(self, msg):
+        init = 0
+        alfa1 = alfa2 = alfa3 = alfa4 = 3.0
+
+        """Compute motion of robot from the last odometry message to the new odometry message"""
+        if init == 0:
+            x = msg.pose.pose.position.x
+            y = msg.pose.pose.position.y
+            z = msg.pose.pose.orientation.z
+            w = msg.pose.pose.orientation.w
+            (t1, t2, theta) = tf.transformations.euler_from_quaternion((0, 0, z, w))
+            lastpose = list([x, y, theta])
+            init = 1
+        if init == 1:
+            z = msg.pose.pose.orientation.z
+            w = msg.pose.pose.orientation.w
+            (t1, t2, theta) = tf.transformations.euler_from_quaternion((0, 0, z, w))
+            dx = msg.pose.pose.position.x - lastpose[0]
+            dy = msg.pose.pose.position.y - lastpose[1]
+            deltatrans = math.sqrt(dx ** 2 + dy ** 2)
+            deltarot1 = math.atan2(dy, dx) - lastpose[2]
+            deltarot2 = lastpose[2] - theta - deltarot1
+            deltarot1prime = deltarot1 - self.sample(alfa1 * deltarot1 + alfa2 * deltatrans)
+            deltatransprime = deltatrans - self.sample(alfa3 * deltatrans + alfa4 * (deltarot1 + deltarot2))
+            deltarot2prime = deltarot2 - self.sample(alfa1 * deltarot2 + alfa2 * deltatrans)
+            poses = PoseArray()
+            # particles.header.frame_id = "map"
+
+            # the for loop in the MCL algorithm goes inside the motionupdate method
+            for i in range(0, nParticles):
+                xprime =lastpose[0] + deltatransprime *math.cos(lastpose[2]+deltarot1prime)
+                yprime =lastpose[1] + deltatransprime *math.sin(lastpose[2]+deltarot1prime)
+                thetaprime =lastpose[2] + deltarot1prime + deltarot2prime
+                self.particle = Particle(self, xprime, yprime, thetaprime)
+
+
+    def sample(self, num):
+
+        #measurement model
 	def weightUpdate(self,msg):
 		self.laser_min_angle = msg.angle_min
 		self.laser_max_angle = msg.angle_max
@@ -87,12 +128,14 @@ class ParticleFilter(object):
 			self.weights.append(q) ##LITT USIKKER PÅ HVORDAN VI SKAL GJØRE DETTE?
 
 
-
 class MCL(object):
 
-	def __init__(self):
+    def __init__(self, xMin, yMin, xMax, yMax, nparticles):
+        rospy.init_node('monteCarlo', anonymous=True)  # Initialize node, set anonymous=true
 
-	#Initialize particle set
+        self.particleFilter = ParticleFilter()
+
+    #Initialize particle set
 	# set number of particles, standard or set
 	#initialize node
 	#subscribe data we need
@@ -105,20 +148,34 @@ class MCL(object):
 	#output Xt -> input for next iteration
 
 
-	#xtm = sample_motion_model(ut,xt-1m)
-	#publish
-	#wtm = measurementmodel(zt,xtm,m)
+    self.posePublisher = rospy.Publisher("Poses", Pose)  # pulisher of position+orioentation to topic poses, type Poses
+    self.particlesPublisher = rospy.Publisher("PoseArrays", poseArray)  # publisher of particles in poseArray
+    rospy.Subscriber("/RosAria/pose", Odometry, particleFilter.motionUpdate)  # subscriber for odometry to be used for motionupdate
+    rospy.Subscriber("/scan", LaserScan, particleFilter.weightUpdate)  # subscribe to kinect scan for the sensorupdate
+    rospy.spin()
 
-	self. # pulisher of poses type Poses
-	self.particlesPublisher = rospy.Publisher("particles", poseArray)
-	rospy.Subscriber("/RosAria/pose", Odometry, motionupdate)
-	rospy.Subscriber("/scan", LaserScan, sensorupdate)
-	rospy.spin()
+    def sample(self,num):
 
 
-	def motionupdate(self, ):
+    def runMCL(self)):
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            # Publish particles to filter in rviz
+            rate.sleep()
 
 
-if __name__ == "__main__":
-	mcl= MCL()
-	#initialize MCL
+if __name__ =="__main__":
+
+    # Map boundaries
+    xMin = -30
+    xMax = 30
+    yMin = -30
+    yMax = 30
+
+
+    # Set number of particles
+    nParticles = 100
+
+    # Initialize MCL
+    monteCarlo = MCL(xMin, yMin, xMax, yMax, nParticles)
+    monteCarlo.runMCL()
